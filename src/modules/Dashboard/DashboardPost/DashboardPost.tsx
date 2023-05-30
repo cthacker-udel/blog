057edebc898b1f5d13bdeba6f2a564e4429a72c0
cthacker-udel <cthacker@udel.edu>
2023-05-30 11:45:37 -0400

    [main] Added post display

--- src/modules/Dashboard/DashboardPost/DashboardPost.tsx
+++ src/modules/Dashboard/DashboardPost/DashboardPost.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { OverlayTrigger } from "react-bootstrap";
+import type { OverlayInjectedProps } from "react-bootstrap/esm/Overlay";
 
 import type { Post } from "@/@types";
+import { generateTooltip } from "@/common";
 
 import styles from "./DashboardPost.module.css";
 
@@ -15,10 +18,59 @@ export const DashboardPost = ({
     _id,
     author,
     createdAt,
-    content,
+    content: _content,
     modifiedAt,
     textContent,
     title,
 }: DashboardPostProperties): JSX.Element => (
-    <div className={styles.each_post}>{title}</div>
+    <div className={styles.each_post}>
+        <div className={styles.each_post_headline}>
+            <div className={styles.each_post_title}>{title}</div>
+            <div className={styles.each_post_description}>{textContent}</div>
+        </div>
+        <div className={styles.each_post_information}>
+            <div className={styles.each_post_singular_info}>
+                <OverlayTrigger
+                    overlay={(properties: OverlayInjectedProps): JSX.Element =>
+                        generateTooltip({
+                            content: "Author",
+                            props: properties,
+                        })
+                    }
+                    placement="left"
+                >
+                    <i className="fa-solid fa-user" />
+                </OverlayTrigger>
+                <span>{author.toString()}</span>
+            </div>
+            <div className={styles.each_post_singular_info}>
+                <OverlayTrigger
+                    overlay={(properties: OverlayInjectedProps): JSX.Element =>
+                        generateTooltip({
+                            content: "Created",
+                            props: properties,
+                        })
+                    }
+                    placement="left"
+                >
+                    <i className="fa-solid fa-calendar-plus" />
+                </OverlayTrigger>
+                <span>{new Date(createdAt).toLocaleString()}</span>
+            </div>
+            <div className={styles.each_post_singular_info}>
+                <OverlayTrigger
+                    overlay={(properties: OverlayInjectedProps): JSX.Element =>
+                        generateTooltip({
+                            content: "Modified",
+                            props: properties,
+                        })
+                    }
+                    placement="left"
+                >
+                    <i className="fa-solid fa-user-pen" />
+                </OverlayTrigger>
+                <span>{new Date(modifiedAt).toLocaleString()}</span>
+            </div>
+        </div>
+    </div>
 );
